f707260ca66d503d1086415bf900fcb2f7fef229
ziggi <borodin@unix7.org>
2018-03-17 19:24:49 +0200

    added ttp229 read code

---
+++ ttp229.c
@@ -0,0 +1,20 @@
+/* $Id$ */
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <twim.h>
+#include <ttp229.h>
+
+
+#define TTP229_ADDR  0x57
+
+uint16_t ttp229_read(void) {
+    volatile uint8_t dat0, dat1;
+    i2c_start_wait((TTP229_ADDR << 1) | I2C_READ);
+    dat0 = i2c_readAck();
+    dat1 = i2c_readNak();
+    i2c_stop();
+    return (dat0 << 8) | dat1;
+}
+/* EOF */
---
+++ ttp229.h
@@ -0,0 +1,13 @@
+/* $Id$ */
+
+#ifndef TTP229_H_IUI
+#define TTP229_H_IUI
+
+#define TTP229_ADDR  0x57
+
+uint16_t ttp229_read(void);
+
+
+
+#endif
+/* EOF */
